cd140f5b1a4f1517279040145ff2696c6d91ac22
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-11 21:53:06 +0600

    room model defined

---
+++ src/app/modules/Room/room.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose';
+import { TRoom } from './room.interface';
+
+const roomSchema = new Schema<TRoom>({
+  name: { type: String, required: true },
+  roomNo: { type: Number, required: true, unique: true },
+  floorNo: { type: Number, required: true },
+  capacity: { type: Number, required: true },
+  pricePerSlot: { type: Number, required: true },
+  amenities: [{ type: String, required: true }],
+  isDeleted: { type: Boolean, default: false },
+});
+
+export const Room = model<TRoom>('Room', roomSchema);
