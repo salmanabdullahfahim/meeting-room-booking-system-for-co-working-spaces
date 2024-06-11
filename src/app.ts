65a60851061fdc9814a96c6a923e8c969d174d26
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-11 20:47:25 +0600

    basic application setup done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 const app: Application = express();
-const port = 3000;
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
+// routes
+app.use('/api', router);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello Everyone In Our Co-Working Space!');
 });
 
 export default app;
