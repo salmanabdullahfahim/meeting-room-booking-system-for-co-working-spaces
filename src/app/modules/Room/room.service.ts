d8984f0eefd63f705f696a606699ba7627166d40
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-11 22:54:18 +0600

    getAllRoomsFromDB and getSingleRoomFromDB added in service

--- src/app/modules/Room/room.service.ts
+++ src/app/modules/Room/room.service.ts
@@ -7,6 +7,18 @@ const createRoomIntoDB = async (payload: TRoom) => {
   return result;
 };
 
+const getAllRoomsFromDB = async () => {
+  const result = await Room.find();
+  return result;
+};
+
+const getSingleRoomFromDB = async (id: string) => {
+  const result = await Room.findById(id);
+  return result;
+};
+
 export const RoomServices = {
   createRoomIntoDB,
+  getAllRoomsFromDB,
+  getSingleRoomFromDB,
 };
