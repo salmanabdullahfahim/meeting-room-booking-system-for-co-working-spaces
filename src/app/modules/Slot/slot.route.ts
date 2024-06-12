beecccbf18aa545a4cd4f4a9c3d2a0220df51bbb
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-12 08:51:09 +0600

    available slot routes created

--- src/app/modules/Slot/slot.route.ts
+++ src/app/modules/Slot/slot.route.ts
@@ -4,5 +4,6 @@ import { slotController } from './slot.controller';
 const router = express.Router();
 
 router.post('/', slotController.createSlot);
+router.get('/availability', slotController.availableSlot);
 
 export const slotRoutes = router;
