d964d40b28e576250b7ef8dd2e64d8f52582ee87
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-12 20:05:15 +0600

    isConfirmed changed

--- src/app/modules/Booking/booking.model.ts
+++ src/app/modules/Booking/booking.model.ts
@@ -7,7 +7,11 @@ const bookingSchema = new Schema<TBooking>({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: String, required: true },
   totalAmount: { type: Number, required: true },
-  isConfirmed: { type: String, enum: ['confirmed', 'unconfirmed', 'canceled'] },
+  isConfirmed: {
+    type: String,
+    enum: ['confirmed', 'unconfirmed', 'canceled'],
+    default: 'unconfirmed',
+  },
   isDeleted: { type: Boolean, default: false },
 });
 
