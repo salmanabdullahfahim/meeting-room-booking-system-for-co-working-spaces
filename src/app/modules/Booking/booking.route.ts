0ee6fe45c8380fa0f1de16a93d2c07a3d514b0e9
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-13 23:01:16 +0600

    add authorization in create booking route

--- src/app/modules/Booking/booking.route.ts
+++ src/app/modules/Booking/booking.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../utils/validateRequest';
 import { bookingValidations } from './booking.validation';
 import { bookingController } from './booking.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLE.user),
   validateRequest(bookingValidations.createBookingValidationSchema),
   bookingController.createBooking,
 );
