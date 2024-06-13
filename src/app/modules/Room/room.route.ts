11c9bbc1d33ad8eaff25da37abe82c80e663157f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-13 15:54:34 +0600

    authorization added in room route

--- src/app/modules/Room/room.route.ts
+++ src/app/modules/Room/room.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../utils/validateRequest';
 import { roomValidations } from './room.validation';
 import { roomController } from './room.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(USER_ROLE.admin),
   validateRequest(roomValidations.createRoomValidationSchema),
   roomController.createRoom,
 );
@@ -17,10 +20,11 @@ router.get('/:id', roomController.getSingleRoom);
 
 router.put(
   '/:id',
+  auth(USER_ROLE.admin),
   validateRequest(roomValidations.updateRoomValidationSchema),
   roomController.updateRoom,
 );
 
-router.delete('/:id', roomController.deleteRoom);
+router.delete('/:id', auth(USER_ROLE.admin), roomController.deleteRoom);
 
 export const roomRoutes = router;
