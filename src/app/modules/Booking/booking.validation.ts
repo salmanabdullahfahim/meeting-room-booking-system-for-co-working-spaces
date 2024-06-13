2206e5292261c9d725d2b29ff6f96631ecbad2a9
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-13 22:56:10 +0600

    booking validation date validate

--- src/app/modules/Booking/booking.validation.ts
+++ src/app/modules/Booking/booking.validation.ts
@@ -1,12 +1,20 @@
 import { z } from 'zod';
 
+const dateValidationRegex = /^\d{4}-\d{2}-\d{2}$/;
 const createBookingValidationSchema = z.object({
   body: z.object({
     room: z.string({ required_error: 'Room is required' }),
     slots: z.array(z.string({ required_error: 'Slot is required' })),
     user: z.string({ required_error: 'User is required' }),
-    date: z.string({ required_error: 'Date is required' }),
-    totalAmount: z.number({ required_error: 'Total Amount is required' }),
+    date: z
+      .string({ required_error: 'Date is required' })
+      .refine(
+        (val) =>
+          dateValidationRegex.test(val) && !isNaN(new Date(val).getTime()),
+        {
+          message: 'Invalid date format, please use YYYY-MM-DD',
+        },
+      ),
   }),
 });
 
