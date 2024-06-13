8a516e34df5a6167a857e1e3fbc5165de8e9e003
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-13 22:17:23 +0600

    available slot and slot validation fixed

--- src/app/modules/Slot/slot.route.ts
+++ src/app/modules/Slot/slot.route.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import { slotController } from './slot.controller';
 import auth from '../../middleware/auth';
 import { USER_ROLE } from '../User/user.constant';
+import validateRequest from '../../utils/validateRequest';
+import { slotValidations } from './slot.validation';
 
 const router = express.Router();
 
-router.post('/', auth(USER_ROLE.admin), slotController.createSlot);
+router.post(
+  '/',
+  auth(USER_ROLE.admin),
+  validateRequest(slotValidations.createSlotValidationSchema),
+  slotController.createSlot,
+);
 router.get('/availability', slotController.availableSlots);
 
 export const slotRoutes = router;
--- src/app/modules/Slot/slot.validation.ts
+++ src/app/modules/Slot/slot.validation.ts
@@ -1,11 +1,35 @@
 import { z } from 'zod';
 
+const dateValidationRegex = /^\d{4}-\d{2}-\d{2}$/;
+
 const createSlotValidationSchema = z.object({
   body: z.object({
     room: z.string({ required_error: 'Room is required' }),
-    date: z.string({ required_error: 'Date is required' }),
-    startTime: z.string({ required_error: 'Start Time is required' }),
-    endTime: z.string({ required_error: 'End Time is required' }),
+    date: z
+      .string({ required_error: 'Date is required' })
+      .refine(
+        (val) =>
+          dateValidationRegex.test(val) && !isNaN(new Date(val).getTime()),
+        {
+          message: 'Invalid date format, please use YYYY-MM-DD',
+        },
+      ),
+    startTime: z.string({ required_error: 'Start Time is required' }).refine(
+      (val) => {
+        return /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/.test(val);
+      },
+      {
+        message: 'Invalid start time format',
+      },
+    ),
+    endTime: z.string({ required_error: 'End Time is required' }).refine(
+      (val) => {
+        return /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/.test(val);
+      },
+      {
+        message: 'Invalid end time format',
+      },
+    ),
   }),
 });
 
