09f78a2e40b1b4c6d4436740a655aa8a665126a3
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-14 11:56:33 +0600

    create booking service created

--- src/app/modules/Booking/booking.service.ts
+++ src/app/modules/Booking/booking.service.ts
@@ -1,9 +1,114 @@
+import { startSession } from 'mongoose';
 import { TBooking } from './booking.interface';
 import { Booking } from './booking.model';
+import { Room } from '../Room/room.model';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import { User } from '../User/user.model';
+import { Slot } from '../Slot/slot..model';
 
-const createBookingIntoDB = async (payload: TBooking) => {
-  const result = await Booking.create(payload);
-  return result;
+const createBookingIntoDB = async (
+  payload: Omit<TBooking, 'totalAmount'>,
+): Promise<TBooking> => {
+  const { room, slots, user, isConfirmed, date } = payload;
+
+  const session = await startSession();
+  session.startTransaction();
+
+  try {
+    const roomInfo = await Room.findById(room).session(session);
+    if (!roomInfo) {
+      throw new Error('Room not found');
+    }
+
+    const isRoomDeleted = roomInfo?.isDeleted;
+
+    if (isRoomDeleted) {
+      throw new AppError(httpStatus.NOT_FOUND, 'Room is deleted!');
+    }
+
+    const userInfo = await User.findOne({ _id: user, role: 'user' }).session(
+      session,
+    );
+    if (!userInfo) {
+      throw new Error('User not found');
+    }
+
+    // Check if slots array is null or empty
+    if (!(slots?.length ?? false)) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Slots cannot be empty');
+    }
+
+    const slotDocuments = await Slot.find({
+      _id: { $in: slots },
+      room,
+      isBooked: false,
+    });
+    if (slotDocuments.length !== slots.length) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'One or more slots are already booked or do not exist',
+      );
+    }
+
+    const pricePerSlot = roomInfo.pricePerSlot;
+    const totalAmount = slots.length * pricePerSlot;
+
+    const booking = await Booking.create(
+      [
+        {
+          room,
+          date,
+          slots,
+          user,
+          totalAmount,
+          isConfirmed,
+        },
+      ],
+      { session },
+    );
+
+    if (!booking.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create booking');
+    }
+
+    const slotUpdate = await Slot.updateMany(
+      { _id: { $in: slots } },
+      { isBooked: true },
+      { session },
+    );
+
+    if (!slotUpdate) {
+      throw new AppError(
+        httpStatus.INTERNAL_SERVER_ERROR,
+        'Failed to create update slot',
+      );
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    const slotsDetail = await Promise.all(
+      [...slots].map((slot) => Slot.findById(slot).exec()),
+    );
+
+    const populatedBooking = await Booking.findById(booking[0]._id)
+      .populate('room')
+      .populate({
+        path: 'user',
+        select: '-password -__v',
+      })
+      .exec();
+
+    // @ts-ignore
+    populatedBooking.slots = slotsDetail;
+
+    return populatedBooking as TBooking;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create booking');
+  }
 };
 
 export const bookingService = {
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { roomRoutes } from '../modules/Room/room.route';
 import { slotRoutes } from '../modules/Slot/slot.route';
 import { authRoutes } from '../modules/Auth/auth.route';
+import { bookingRoutes } from '../modules/Booking/booking.route';
 
 const router = Router();
 
 const moduleRoutes = [
   { path: '/rooms', route: roomRoutes },
   { path: '/slots', route: slotRoutes },
+  { path: '/bookings', route: bookingRoutes },
   { path: '/auth', route: authRoutes },
 ];
 
