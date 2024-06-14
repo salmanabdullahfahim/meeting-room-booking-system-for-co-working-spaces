ae15776ce381b64c45526a97cdd0d3f0111f109f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-06-14 12:04:33 +0600

    get all bookings controller created

--- src/app/modules/Booking/booking.controller.ts
+++ src/app/modules/Booking/booking.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { bookingService } from './booking.service';
+import { sendNoDataFoundResponse } from '../../utils/noDataResponse';
 
 const createBooking = catchAsync(async (req, res) => {
   const result = await bookingService.createBookingIntoDB(req.body);
@@ -13,6 +14,20 @@ const createBooking = catchAsync(async (req, res) => {
   });
 });
 
+const getAllBooking = catchAsync(async (req, res) => {
+  const result = await bookingService.getAllBookingFromDB();
+  if (result.length === 0) {
+    return sendNoDataFoundResponse(res);
+  }
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'All bookings retrieved successfully',
+    data: result,
+  });
+});
+
 export const bookingController = {
   createBooking,
+  getAllBooking,
 };
